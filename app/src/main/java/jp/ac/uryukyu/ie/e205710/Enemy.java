f5433dc9ec2677507454084704c62b11063c997f
Ryuya Naha <e205710@ie.u-ryukyu.ac.jp>
2022-02-06 16:59:40 +0900

    修正したattack()

--- app/src/main/java/jp/ac/uryukyu/ie/e205710/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205710/Enemy.java
@@ -55,11 +55,13 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
-
+        if (hitPoint < 0){
+            attack = 0;
+        }else{
         int damage = (int)(Math.random() * attack);
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
-        
+        }
     }
 
     /**
